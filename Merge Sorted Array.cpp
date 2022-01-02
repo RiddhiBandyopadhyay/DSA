a20333b8d62bdba8386125237c4fe0f34709d9ff
Riddhi <riddhibanerjee6@gmail.com>
2022-01-02 16:38:34 +0530

    second code

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        
+        while(i>=0 and j>=0){
+            if(i>=0 and nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+    
+        while(j>=0){
+             nums1[k--] = nums2[j--];
+        }
+        while(i>=0){
+             nums1[k--] = nums1[i--];
+        }
+    }
+};
+
+
+//Problem - 2 : In-place merge two sorted array
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Utility function to print contents of an array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+ 
+// Function to in-place merge two sorted arrays X[] and Y[]
+// invariant: `X[]` and `Y[]` are sorted at any point
+void merge(int X[], int Y[], int m, int n)
+{
+    // Consider each element `X[i]` of array `X` and ignore the element if it is
+    // already in the correct order; otherwise, swap it with the next smaller
+    // element, which happens to be the first element of `Y`.
+    for (int i = 0; i < m; i++)
+    {
+        // compare the current element of `X[]` with the first element of `Y[]`
+        if (X[i] > Y[0])
+        {
+            swap(X[i], Y[0]);
+            int first = Y[0];
+ 
+            // move `Y[0]` to its correct position to maintain the sorted
+            // order of `Y[]`. Note: `Y[1…n-1]` is already sorted
+            int k;
+            for (k = 1; k < n && Y[k] < first; k++) {
+                Y[k - 1] = Y[k];
+            }
+ 
+            Y[k - 1] = first;
+        }
+    }
+}
+ 
+int main()
+{
+    int X[] = { 1, 4, 7, 8, 10 };
+    int Y[] = { 2, 3, 9 };
+ 
+    int m = sizeof(X) / sizeof(X[0]);
+    int n = sizeof(Y) / sizeof(Y[0]);
+ 
+    merge(X, Y, m, n);
+ 
+    cout << "X: "; printArray(X, m);
+    cout << "Y: "; printArray(Y, n);
+ 
+    return 0;
+}
